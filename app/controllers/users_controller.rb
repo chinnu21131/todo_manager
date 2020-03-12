b5301a6388f55e9501c670926538dae5f9868bb3
chinnu21131 <gvkprasad2001@gmail.com>
2020-03-12 13:45:29 +0530

    Users table creatend and a create is written in post convention

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
-    render plain: User.all.to_a
+    render plain: User.all.map { |user| user.to_pleasant_string }.join("\n")
+  end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create!(name: name, email: email, password: password)
+    render plain: "user created with id #{new_user.id}"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  def to_pleasant_string
+    "name : #{name} \t email : #{email} \t password : #{password}"
+  end
 end

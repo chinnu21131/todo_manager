b8b28290fd4b94c6d74bb5f599ff519747766629
chinnu21131 <gvkprasad2001@gmail.com>
2020-04-14 13:05:47 +0530

    Updated database migrations

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,48 @@
 class Todo < ActiveRecord::Base
-  def to_pleasant_string
-    is_completed = completed ? "[x]" : "[ ]"
-    "#{id} #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
+  def due_today
+    due_date == Date.today
+  end
+
+  def self.overdue
+    all.where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
+
+  def self.show_list
+    puts "My Todo-list\n\n"
+
+    puts "Overdue"
+    puts Todo.where("due_date < ?", Date.today).to_displayable_list
+    puts "\n\n"
+
+    puts "Due Today"
+    puts Todo.where("due_date = ?", Date.today).to_displayable_list
+    puts "\n\n"
+
+    puts "Due Later"
+    puts Todo.where("due_date > ?", Date.today).to_displayable_list
+    puts "\n\n"
+  end
+
+  #def self.add_task(task)
+  #  Todo.create!(todo_text: task[:todo_text], due_date: Date.today + task[:due_in_days], completed: false)
+  #end
+
+  def self.mark_as_complete!(todo_id)
+    todo = Todo.find(todo_id)
+    todo.completed = true
+    todo.save
+    return todo
   end
 end

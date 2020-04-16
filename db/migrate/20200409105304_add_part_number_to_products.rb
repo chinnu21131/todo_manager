18f5cda28a0d86b158830e9c52deef3e5d2d1b34
chinnu21131 <gvkprasad2001@gmail.com>
2020-04-16 10:45:02 +0530

    todos migrate fie added

--- db/migrate/20200409105304_add_part_number_to_products.rb
+++
@@ -1,4 +0,0 @@
-class AddPartNumberToProducts < ActiveRecord::Migration[6.0]
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_122026) do
+ActiveRecord::Schema.define(version: 2020_04_15_113144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

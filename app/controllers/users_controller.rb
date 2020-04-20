d437ce609dfb394fe2d7b531107c432ee0efaa2f
chinnu21131 <gvkprasad2001@gmail.com>
2020-04-20 23:04:18 +0530

    User sign-up created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,14 @@
 class UsersController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
-  def index
-    render plain: User.all.map { |user| user.to_pleasant_string }.join("\n")
+  def new
+    render "users/new"
   end
 
   def create
-    name = params[:name]
-    email = params[:email]
-    password = params[:password]
-    new_user = User.create!(name: name, email: email, password: password)
-    render plain: "user created with id #{new_user.id}"
-  end
-
-  def login
-    render plain: User.where("email = ? and password = ?", params[:email], params[:password]).exists?
+    User.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+    )
+    redirect_to "/"
   end
 end

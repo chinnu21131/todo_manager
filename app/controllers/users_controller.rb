1cc696009a8386b6413f14b2991340c38d67776c
chinnu21131 <gvkprasad2001@gmail.com>
2020-04-22 04:19:05 +0530

    active record validations added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,12 @@ class UsersController < ApplicationController
   skip_before_action :ensure_user_logged_in
 
   def create
-    User.create(
-      first_name: params[:first_name],
-      last_name: params[:last_name],
-      email: params[:email],
-      password: params[:password],
-    )
-    redirect_to "/"
+    user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password])
+    if user.save
+      redirect_to "/"
+    else
+      flash[:error] = user.errors.full_messages.join(" , ")
+      redirect_to new_user_path
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :password, length: { minimum: 6 }
   has_secure_password
   has_many :todos
 end
